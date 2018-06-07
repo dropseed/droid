bb25d9d97bea3d706aabe79d6de4765d6defd87c
Dave Gaeddert <dave.gaeddert@gmail.com>
2018-06-07 10:25:23 -0500

    Back to development: 0.1.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name='droid',
-    version='0.1.0',
+    version='0.1.1.dev0',
     description='',
     # long_description=readme,
     long_description_content_type='text/markdown',
