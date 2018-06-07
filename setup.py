c95d6b4a8ca13cf345079399e5668e3a3822dc58
Dave Gaeddert <dave.gaeddert@gmail.com>
2018-06-07 10:23:04 -0500

    prepare for release

--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 # with open('README.rst') as f:
 #     readme = f.read()
 #
-# with open('LICENSE') as f:
-#     license = f.read()
 
 setup(
     name='droid',
     version='0.1.0',
     description='',
     # long_description=readme,
+    long_description_content_type='text/markdown',
+    url='https://github.com/dropseed/droid',
     author='Dropseed, LLC',
     author_email='python@dropseed.io',
+    python_requires='>=3.6.0',
     install_requires=(
         'configyaml==0.4.0',
         'click',
@@ -31,7 +32,15 @@ setup(
         'traitlets',
         'slackclient',
     ),
-    url='https://github.com/dropseed/droid',
-    # license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
+    py_modules=['droid'],
+    include_package_data=True,
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
 )
